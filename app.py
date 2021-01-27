6435f4e3f13a743e4754362b2031a647c8f203bf
theogcrafted <77366643+theogcrafted@users.noreply.github.com>
2021-01-26 20:02:31 -0500

    Update app.py

--- app.py
+++ app.py
@@ -25,14 +25,6 @@ class Book(db.Model):
 
 
 
-
-class BlogPost(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    title = db.Column(db.String(100), nullable=False)
-    content = db.Column(db.Text, nullable=False)
-    author = db.Column(db.String(50), nullable=False, default = 'N/A')
-    date_posted = db.Column(db.DateTime, nullable=False, default=datetime.utcnow)
-
     def __repr__(self):
         return 'Blog post ' + str(self.id)
 
