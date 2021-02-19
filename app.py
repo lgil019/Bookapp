6bbd5ec95f3ee5696d3fbde9e0312e815460765e
lgil019 <77906774+lgil019@users.noreply.github.com>
2021-02-18 22:27:51 -0500

    shopping cart html, most of the functionality for the shopping cart class, also a db.model.

--- app.py
+++ app.py
@@ -9,10 +9,30 @@ class ShoppingCart(object):
         self.books = {}
 
     def add_item(self, book_name, quantity, price):
+        self.total += (quantity * price)
+        self.books = (book_name : quantity)
+
+    def remove_item(self, book_name, quantity, price):
         self.total -= (quantity * price)
+        if quantity > self.books[book_name]
+           del self.books[book_name]
+        self.books[book_name] -= quantity
 
-    def remove_item(self, book_name, quantity, price)
+    def checkout()
+        balance = 0
+        if cash_paid < self.total:
+          return "Not enough funds for purchase."
+        balance = cash_paid - self.total
+          return balance
 
+class Shop(ShoppingCart):
+
+    def __init__(self):
+      ShoppingCart.__init__(self)
+      self.quantity = 100
+
+    def remove_item(self):
+        self.quantity -= 1
 
 db = SQLAlchemy(app)
 
@@ -24,11 +44,18 @@ class Orders(db.Model)
     customer_id = db.Column(db.integer, nullable = False)
     payment_id = db.Column(db.integer, nullable = False)
     time_stamp = db.Column(db.datetime, nullable = False, default=datetime.utcnow)
-    id = db.Column(db.integer, nullable = False)
+    title = db.Column(db.integer, nullable = False)
     quantity = db.Column(db.integer, nullable = False, default = 0)
     total = db.Column(db.integer, nullable = False, default = 0)
 
-class Saved_items(db.Model)
+@app.route('/shoppingcart', methods=['GET'])
+
+class ShoppingCart(db.Model)
+
+    title = db.Column(db.String(30), nullable=False)
+    price = db.Column(db.integer, nullable = False, default = 0)
+    quantity = db.Column(db.integer, nullable = False, default = 0)
+    total = db.Column(db.integer, nullable = False, default = 0)
 
 
     
