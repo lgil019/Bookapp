adff427209bdaa20388b893455f72d436c607aba
jsgazsi <31450665+jsgazsi@users.noreply.github.com>
2021-02-22 11:04:43 -0500

    Delete tables.py

--- tables.py
+++
@@ -1,7 +0,0 @@
-from flask_table import Table, Column
-
-class Results(Table):
-    id = Column('id', show=False)
-    title = Column('title')
-    author = Column('author')
-    date_published = Column('date published')
