406c8ae6703413abaa1047424b9142fd800c8128
lgil019 <77906774+lgil019@users.noreply.github.com>
2021-03-11 14:41:05 -0500

    used joses codebase, comment box pops up.

---
+++ CEN4010Project/bookstore/__init__.py
@@ -0,0 +1,27 @@
+import bcrypt
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from flask_bcrypt import Bcrypt
+from flask_login import LoginManager, LoginManager 
+from flask_mail import Mail
+
+app = Flask(__name__)
+#app.secret_key = 'monkey'
+#Saw this way to set secret key
+app.config['SECRET_KEY'] = 'monkey'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///database.sqlite3'
+db = SQLAlchemy(app)
+bcrypt = Bcrypt(app)
+login_manager = LoginManager(app)
+login_manager.login_view = 'login'
+login_manager.login_message_category = 'info'
+app.config['MAIL_SERVER'] = 'smtp.googlemail.com'
+app.config['MAIL_PORT'] = 587
+app.config['MAIL_USE_TLS'] = True
+app.config['MAIL_USERNAME'] = 'cen4010group11@gmail.com'
+app.config['MAIL_PASSWORD'] = 'monkeyseemonkeydo'
+mail = Mail(app)
+
+from bookstore import routes
+
+
